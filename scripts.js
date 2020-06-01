4914760350c2ceb3de34dd2966601301786bb820
godigitinsurance <59437973+godigitinsurance@users.noreply.github.com>
2020-06-01 16:35:19 +0530

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,13 @@
+$("#customerName").text("Suraj Kumar");
+$("#policyNumber").text("AI06259685");
+$("#startDate").text("02-Jun-2020");
+$("#endDate").text("01-Jun-2021");
+
+
+$("#type").text("Two Wheeler");
+$("#registrationNumber").text("PB08AM2546");
+$("#make").text("HERO HONDA");
+$("#model").text("PASSION PLUS");
+$("#vehicalSubType").text("STD");
+$("#engineNo").text("13513");
+$("#chassis").text("11708");
